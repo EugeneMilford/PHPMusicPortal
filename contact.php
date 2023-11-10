c8ba9deccbc328779a2d71a4b89f5c62aeb5bcdb
Eugene Milford <gmilford321@gmail.com>
2023-11-10 20:37:32 +0200

    Pages Added

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Music Portal</title>
+    </head>
+    <body>
+        <p>Music Portal Contact page</p>
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Music Portal</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <p>Music Portal index page</p>
+        
+    </body>
+</html>
+
